94124d0e01caba376573e18ab0990fb0ecd992cd
Akmaljon571 <ahmadjonovakmaljon571@yandex.ru>
2022-04-03 14:10:11 +0300

    javascript

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let random2 =  Math.round(Math.random()*100);
+
+console.log(random2);
+
+if (random2 >=1 && random2 <=17){
+    console.log("maktab yoshi");
+}  else  if (random2 >=18 && random2 <=24) {
+    console.log("Universitet yoshi")
+}
+ else if (random2 >=25 && random2 <=40){
+     console.log("ishlash yoshi")
+} else if (41 <=random2 ){
+    console.log("Nuroniylar yoshi")
+}
+
+
+
